ab603290c3e5eca337d3079b777de0b2b7daa294
isamyakt <isamyakt17@gmail.com>
2023-08-16 01:38:27 +0530

    e2e test for start claim  and claim rewards

--- contract/lib.rs
+++ contract/lib.rs
@@ -16,13 +16,9 @@ pub mod magink {
     pub struct Magink {
         user: Mapping<AccountId, Profile>,
     }
-    #[derive(
-        Debug, PartialEq, Eq, PartialOrd, Ord, Clone, scale::Encode, scale::Decode,
-    )]
-    #[cfg_attr(
-        feature = "std",
-        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
-    )]
+
+    #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, scale::Encode, scale::Decode,)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout))]
     pub struct Profile {
         // duration in blocks until next claim
         claim_era: u8,
@@ -174,6 +170,96 @@ pub mod magink {
         }
         fn advance_block() {
             ink::env::test::advance_block::<ink::env::DefaultEnvironment>();
+        }
+    }
+
+    #[cfg(all(test, feature = "e2e-tests"))]
+    mod e2e_tests {
+        use super::*;
+        use ink::primitives::AccountId;
+        use ink_e2e::build_message;
+        
+        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+        
+        const ERA: u8 = 10; // Era duration in blocks
+        
+        #[ink_e2e::test]
+        async fn start_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+            let mut magink = MaginkRef::new(); // Instantiate Magink contract
+        
+            // Start the era for the caller
+            let start_message = build_message::<MaginkRef>(magink.account_id())
+                .call(|magink| magink.start(ERA));
+            client
+                .call(&ink_e2e::alice(), start_message, 0, None)
+                .await
+                .expect("start message failed");
+        
+            // Verify remaining blocks in the era
+            let remaining = magink.get_remaining();
+            assert_eq!(remaining, ERA);
+        
+            // Advance the block and check remaining again
+            advance_block(&mut client, &magink).await;
+            let remaining_after_advance = magink.get_remaining();
+            assert_eq!(remaining_after_advance, ERA - 1);
+        
+            Ok(())
+        }
+        
+        #[ink_e2e::test]
+        async fn claim_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+            let mut magink = MaginkRef::new(); // Instantiate Magink contract
+            magink.start(ERA); // Start the era
+        
+            advance_n_blocks(&mut client, &magink, ERA - 1).await; // Advance blocks
+        
+            // Claim badge fails, too early
+            let claim_result = magink.claim();
+            assert_eq!(claim_result, Err(Error::TooEarlyToClaim));
+        
+            // Advance the block and claim the badge
+            advance_block(&mut client, &magink).await;
+            let claim_result = magink.claim();
+            assert_eq!(claim_result, Ok(()));
+        
+            // Verify badge count for the caller
+            let badge_count = magink.get_badges();
+            assert_eq!(badge_count, 1);
+        
+            // Verify badge count for Alice's account
+            let badge_count_for_alice = magink.get_badges_for(ink_e2e::alice());
+            assert_eq!(badge_count_for_alice, 1);
+        
+            // Verify remaining blocks after claiming
+            let remaining_after_claim = magink.get_remaining();
+            assert_eq!(remaining_after_claim, ERA);
+        
+            Ok(())
+        }
+        
+        // Helper function to advance the block
+        async fn advance_block(
+            client: &mut ink_e2e::Client<C, E>,
+            contract: &MaginkRef,
+        ) {
+            let advance_message = build_message::<MaginkRef>(contract.account_id())
+                .call(|magink| magink.get_remaining());
+            client
+                .call(&ink_e2e::alice(), advance_message, 0, None)
+                .await
+                .expect("advance block message failed");
+        }
+        
+        // Helper function to advance n blocks
+        async fn advance_n_blocks(
+            client: &mut ink_e2e::Client<C, E>,
+            contract: &MaginkRef,
+            n: u8,
+        ) {
+            for _ in 0..n {
+                advance_block(client, contract).await;
+            }
         }
     }
 }
